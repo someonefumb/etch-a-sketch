b1cef87ec533f03ab064236dd4f9df5b1f1e5d45
someone_fumb <someonefumb@gmail.com>
2023-07-13 17:31:33 -0400

    Add a drawing feature

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 // Grid
 const grid = document.querySelector('.grid');
 
+// Tell if mouse is down
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 // Create Grid
 function createGrid (boxNum) {
     for (i = 0; i < (boxNum*boxNum); i++) {
         const blocks = document.createElement('div');
-        blocks.classList.add('grid-blocks')
+        blocks.classList.add('grid-blocks');
+        blocks.addEventListener('mouseover', draw)
+
+        function draw (e) {
+            if (e.type === 'mouseover' && !mouseDown) {
+                return;
+            } else {
+                blocks.setAttribute('style', 'background-color: black');
+            }
+        }
+
         grid.appendChild(blocks);
     }
     grid.setAttribute('style', `grid-template-columns: repeat(${boxNum}, 1fr)`);
 }
 
-createGrid(4);
+createGrid(16);
