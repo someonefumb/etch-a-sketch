bab42e1f74a9e1ac2308b3913185951d3b6cacd7
someone_fumb <someonefumb@gmail.com>
2023-07-13 19:27:04 -0400

    Add button options for color

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
 // Grid
 const grid = document.querySelector('.grid');
 
+// Colors
+let blackColor = true;
+let rainbowColor = false;
+let monotoneColor = false;
+let eraserColor = false;
+
+// Buttons
+const gridSizeBtn = document.querySelector('#gridSizeBtn');
+let blackBtn = document.querySelector('#blackBtn');
+const rainbowBtn = document.querySelector('#rainbowBtn');
+const monotoneBtn = document.querySelector('#monotoneBtn');
+const eraserBtn = document.querySelector('#eraserBtn');
+const resetBtn = document.querySelector('#resetBtn');
+
+const btns = document.querySelectorAll('button');
+btns.forEach((button) => {
+    button.addEventListener('click', () => {
+        blackColor = false;
+        rainbowColor = false;
+        monotoneColor = false;
+        eraserColor = false;
+        switch(button.id) {
+            case 'blackBtn':
+                blackColor = true;
+                break;
+            case 'rainbowBtn':
+                rainbowColor = true;
+                break;
+            case 'monotoneBtn':
+                monotoneColor = true;
+                break;
+            case 'eraserBtn':
+                eraserColor = true;
+                break;
+            case 'gridSizeBtn':
+                console.log('works');
+                break;
+            case 'resetBtn':
+                console.log('works');
+                break;
+        }
+    });
+});
+
 // Tell if mouse is down
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
@@ -17,9 +61,15 @@ function createGrid (boxNum) {
         function draw (e) {
             if (e.type === 'mouseover' && !mouseDown) {
                 return;
-            } else {
+            } else if (blackColor === true) {
                 blocks.setAttribute('style', 'background-color: black');
-            }
+            } else if (rainbowColor === true) {
+                blocks.setAttribute('style', `background-color: ${''}`);
+            }  else if (monotoneColor === true) {
+                blocks.setAttribute('style', `background-color: ${''}`);
+            }  else if (eraserColor === true) {
+                blocks.setAttribute('style', 'background-color: white');
+            } 
         }
 
         // Show color when hovering
